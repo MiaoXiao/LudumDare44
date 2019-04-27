15669ad4bef1f3d9bc4be6c9a9f817f79841f572
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-04-27 01:40:37 -0700

    add testing capabilities

--- LudumDare44/Assets/Scripts/beeColony.cs
+++ LudumDare44/Assets/Scripts/beeColony.cs
@@ -14,20 +14,39 @@ public class beeColony : MonoBehaviour
     private float speed = 1.0f; //****1.0/1 distance / time
     private float energy = 1.5f; //****1.5/1 honeyGathered / honeyTotal
 
-    private int tempRes = 0; //spectrum from 10 to -10, where 10 is a cold bee and -10 is a hot bee
+    private int tempRes = 0; //spectrum from 10 to -10, where 10 is a cold bee group and -10 is a hot bee group
 
     void Awake(){
         
     }
     
     void Update(){
-        
+        if (Input.GetKey("w")){
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + (speed * Time.deltaTime), 0);
+        }
+        if (Input.GetKey("s")){
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + (-1 * speed * Time.deltaTime), 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + (-1 * speed * Time.deltaTime), gameObject.transform.position.y, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + (speed * Time.deltaTime), gameObject.transform.position.y, 0);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            addBees(10);
+        }
     }
 
     public void addBees(int honeyAmount){
         numBees += 1;
-        int honeyCost = 0;
-        honeyCost = honeyAmount + (upgradeCost * numUpgrades);
+        Debug.Log(numBees);
+        int honeyCost = 10;
+        honeyCost += upgradeCost * numUpgrades;
+        Debug.Log(honeyCost);
         //TODO: add subtration functionality here, update grabbed singleton grabbed [honeyAmt - honeyCost]
     }
 }
