da00888059b9a5e40b9ba425736c1d980f6178a3
Joel Lee <lee.joel395@gmail.com>
2019-04-27 02:56:17 -0700

    initial for encounter todo needs some stuff from colony

---
+++ LudumDare44/Assets/Scripts/Encounter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Handles interaction between Colony and Region
+/// </summary>
+public class Encounter : MonoBehaviour
+{
+    beeColony _currentColony = null;
+    Region _currentRegion = null;
+
+    private int potentialHoney = 0;
+
+    public void PopulateInfo(beeColony c, Region r) {
+        _currentColony = c;
+        _currentRegion = r;
+    }
+
+    public void StartEncounter() {
+        if(!_currentColony || !_currentRegion) {
+            return; //no region or colony set
+        }
+
+        StartCoroutine(TickCoroutine());
+    }
+
+    IEnumerator TickCoroutine() {
+        float traveledDist = 0f;
+
+        //travel to the region
+        while (traveledDist < _currentRegion.HiveDistance) {
+            traveledDist += _currentColony.speed * Time.deltaTime;
+
+            yield return 0f;
+
+        }
+
+        //at the cool food place
+        while (_currentColony) {
+
+            yield return 0f;
+        }
+
+        traveledDist = 0;
+        //travel back to hive
+        while (traveledDist < _currentRegion.HiveDistance) {
+            traveledDist += _currentColony.speed * Time.deltaTime;
+
+            yield return 0f;
+
+        }
+
+        yield return 0f;
+    }
+}
