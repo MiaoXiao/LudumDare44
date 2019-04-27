9b30df5091eb726ec0d6d1bb2a20b4622c4f8d65
ricafeng <ricafeng1994@gmail.com>
2019-04-27 16:28:12 -0700

    shop ui

---
+++ LudumDare44/Assets/Scripts/ShopUi.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUi : MonoBehaviour
+{
+    [Header("Traits To Buy")]
+
+    [SerializeField]
+    private Trait[] _allTraits;
+
+    [SerializeField]
+    private int _dnaCost = 250;
+
+    [Header("Purchase Icons")]
+
+    [SerializeField]
+    private Transform _buttonGridLayout;
+
+    [SerializeField]
+    private Button _traitButton;
+
+    [Header("Trait Information")]
+
+    [SerializeField]
+    private Text _selectedTraitName;
+
+    [SerializeField]
+    private Text _selectedTraitDescription;
+
+    [SerializeField]
+    private Text _purchaseText;
+
+    [Header("Toggle")]
+
+    [SerializeField]
+    private Transform _toggleOnPos;
+
+    [SerializeField]
+    private Transform _toggleOffPos;
+
+    [SerializeField]
+    private float _timeToToggle = 1.5f;
+
+    [SerializeField]
+    private Image _toggleImage;
+
+    [SerializeField]
+    private Sprite _activeTab;
+
+    [SerializeField]
+    private Sprite _hiddenTab;
+
+    private bool _toggled = true;
+
+    private Trait _currentlySelectedTrait;
+
+    private bool _toggleInProgress = false;
+
+    private void Awake()
+    {
+        _purchaseText.text = "Purchase for " + _dnaCost + " DNA";
+        GenerateTraitIcons();
+    }
+
+    private void GenerateTraitIcons()
+    {
+        for (int i = 0; i < _allTraits.Length; ++i)
+        {
+            Button newTraitButton = Instantiate(_traitButton, _buttonGridLayout, false);
+            newTraitButton.onClick.AddListener(delegate { SetViewedTrait(_allTraits[i]); });
+        }
+
+        SetViewedTrait(_allTraits[0]);
+    }
+
+    private void SetViewedTrait(Trait t)
+    {
+        _currentlySelectedTrait = t;
+        _selectedTraitName.text = t.name;
+        _selectedTraitDescription.text = t.description;
+    }
+
+    public void PurchaseSelectedTrait()
+    {
+        HiveManager.Instance.ActiveColony.addTrait(_currentlySelectedTrait);
+    }
+
+    public void ToggleShopTab()
+    {
+        if (_toggleInProgress) return;
+
+        _toggled = !_toggled;
+        if (_toggled)
+        {
+            _toggleImage.sprite = _activeTab;
+        }
+        else
+        {
+            _toggleImage.sprite = _hiddenTab;
+        }
+        StartCoroutine(Toggle());
+    }
+
+    private IEnumerator Toggle()
+    {
+        _toggleInProgress = true;
+        float startingTime = 0f;
+        Vector3 starting = transform.position;
+        Vector3 ending = _toggled ? _toggleOnPos.position : _toggleOffPos.position;
+        while (startingTime < _timeToToggle)
+        {
+            startingTime += Time.deltaTime;
+            Vector3 nextPos = Vector3.Slerp(starting, ending, startingTime / _timeToToggle);
+            transform.position = nextPos;
+            yield return null;
+        }
+        transform.position = ending;
+        _toggleInProgress = false;
+    }
+
+}
