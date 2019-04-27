3f5c258b6f959e5cdf034d40f3a47f2a09a36b3d
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-04-27 01:13:59 -0700

    updated colony script with initial variables and added colony scene

--- LudumDare44/Assets/Scripts/beeColony.cs
+++ LudumDare44/Assets/Scripts/beeColony.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class beeColony : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    //Public Field
+    public int numUpgrades = 0;
+
+    //Private field
+    private int numBees = 5;
+    private int upgradeCost = 5;
+
+    private float speed = 1.0f; //****1.0/1 distance / time
+    private float energy = 1.5f; //****1.5/1 honeyGathered / honeyTotal
+
+    private int tempRes = 0; //spectrum from 10 to -10, where 10 is a cold bee and -10 is a hot bee
+
+    void Awake(){
+        
+    }
+    
+    void Update(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void addBees(int honeyAmount){
+        numBees += 1;
+        int honeyCost = 0;
+        honeyCost = honeyAmount + (upgradeCost * numUpgrades);
+        //TODO: add subtration functionality here, update grabbed singleton grabbed [honeyAmt - honeyCost]
     }
 }
