38c0fe3c3626918181ec4c9c375ae608a8cada82
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-04-27 17:48:16 -0700

    add initial values to colony ui handler

--- LudumDare44/Assets/Scripts/ColonyUIHandler.cs
+++ LudumDare44/Assets/Scripts/ColonyUIHandler.cs
@@ -1,8 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ColonyUIHandler : MonoBehaviour
 {
-    
+
+    /// <summary>
+    /// Text are just for displaying the information text. i.e. Honey Level:
+    /// </summary>
+    [SerializeField]
+    private Text _temperatureText;
+
+    [SerializeField]
+    private Text _speedText;
+
+    [SerializeField]
+    private Text _speedUnit;
+
+    /// <summary>
+    /// updated text variables
+    /// </summary>
+    [SerializeField]
+    private Text _numBees;
+
+    [SerializeField]
+    private Sprite _temperatureIcon;
+
+    [SerializeField]
+    private int _currentSpeed;
+
 }
