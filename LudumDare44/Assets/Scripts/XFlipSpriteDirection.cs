e3bc390f8dc422b7c3fc6c07684b3991301cd3a9
ricafeng <ricafeng1994@gmail.com>
2019-04-28 15:09:26 -0700

    pushed animation

---
+++ LudumDare44/Assets/Scripts/XFlipSpriteDirection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class XFlipSpriteDirection : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer _spriteRenderer;
+
+    private float lastPosX;
+
+    private void Awake()
+    {
+        lastPosX = transform.position.x;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        _spriteRenderer.flipX = lastPosX > transform.position.x;
+        lastPosX = transform.position.x;
+    }
+}
