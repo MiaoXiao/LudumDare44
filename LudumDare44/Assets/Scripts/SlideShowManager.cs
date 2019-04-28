558d7fbf17725bf1869e27aa8481580d4f4fc60b
ricafeng <ricafeng1994@gmail.com>
2019-04-28 01:49:06 -0700

    added tutorial scene

---
+++ LudumDare44/Assets/Scripts/SlideShowManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SlideShowManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _allSlides;
+
+    private int currentSlide = 0;
+
+    public void NextSlide()
+    {
+        _allSlides[currentSlide].gameObject.SetActive(false);
+        _allSlides[++currentSlide].gameObject.SetActive(true);
+    }
+
+    public void PrevSlide()
+    {
+        _allSlides[currentSlide].gameObject.SetActive(false);
+        _allSlides[--currentSlide].gameObject.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("hivemanager");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
